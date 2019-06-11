969301616fb41556a91a6153da8bacc1ff419c9d
Kanwar Pal Singh <kanwarpal922@gmail.com>
2019-06-11 20:13:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component{
+
+constructor(props){
+  super(props);
+  this.state = {
+    name: ""
+  }
+
+
+}
+
+changeFunction = (e) => {
+  console.log("hello");
+  console.log(e.target.value);
+
+ if ((e.target.value) > 100) {
+  this.setState({
+    name: "greater than 100"
+  });
+}
+
+else if((e.target.value) == 100){
+  this.setState({
+    name: "century"
+  });
+}
+
+else{
+  this.setState({
+    name: "less than 100"
+  });
+} 
+
+ 
+}
+
+render(){
+
+  return(
+
+<div>
+   <h1> {this.state.name}</h1>
+
+<input type="text" placeholder="enter value" onChange={this.changeFunction}></input>
+   
+
+</div>
+
   );
+}
+
+
+
 }
 
 export default App;
